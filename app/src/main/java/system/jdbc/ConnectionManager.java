6f33df83872104e114a5a6ef986fdbb3abac890a
navneetdavang <navneetdavang@gmail.com>
2021-10-11 13:25:58 +0530

    minor fix

--- app/src/main/java/system/jdbc/ConnectionManager.java
+++ app/src/main/java/system/jdbc/ConnectionManager.java
@@ -7,7 +7,7 @@ public class ConnectionManager {
 	
 	private static final String DRIVER_CLASS = "com.mysql.cj.jdbc.Driver";
 	
-	private static final String CONNECTION_URL = "jdbc:mysql://localhost:3306/pharmacy_mangement_system";
+	private static final String CONNECTION_URL = "jdbc:mysql://localhost:3306/pharmacy_management_system";
 	
 	private static final String USERNAME = "DJMN";
 	
