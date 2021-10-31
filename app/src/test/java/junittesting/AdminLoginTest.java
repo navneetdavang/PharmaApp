b346744423ea7101dd2e2dab6dd0b9544c925644
navneetdavang <navneetdavang@gmail.com>
2021-10-31 18:53:04 +0530

    Added another test in Admin login test suite

--- app/src/test/java/junittesting/AdminLoginTest.java
+++ app/src/test/java/junittesting/AdminLoginTest.java
@@ -8,7 +8,8 @@ import system.services.LoginServices;
 
 public class AdminLoginTest {
   
-	@Test 
+	// test case if the admin is the valid user
+	@Test
 	public void chemistLoginTest() {
 		String username = "admin@chemist";
 		String password = "chemist@1234";
@@ -17,5 +18,17 @@ public class AdminLoginTest {
 		
 		Assert.assertNotEquals(null, chemist);
 		
+	}
+	
+	
+	// login test of the admin if user is invalid
+	@Test 
+	public void chemistInvalidLoginTest() {
+		String username = "nav@chemist";
+		String password = "nav@1234";
+		
+		Chemist chemist = LoginServices.chemistLoginService(username, password);
+		
+		Assert.assertEquals(null, chemist);
 	}
 }
